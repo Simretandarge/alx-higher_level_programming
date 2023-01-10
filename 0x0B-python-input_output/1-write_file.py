da121648b748c43228f579eb8b72846c3eab6d8e
simretandarge <andargesimret@gmail.com>
2023-01-10 11:56:14 +0000

    task 0 and 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-number_of_lines.py
+
+def number_of_lines(filename=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return lines
