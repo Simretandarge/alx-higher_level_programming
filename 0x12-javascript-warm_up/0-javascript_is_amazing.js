5af7c153fec314e2a32a5d9c9df8337eda95b9a1
Simret Andarge <andargesimret@gmail.com>
2023-03-15 02:43:13 -0700

    task zero

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Print string “Javascript is amazing”
 
-const myVar = 'Javascript is amazing';
-console.log('Javascript is amazing');
+const myVar = "avaScript is amazing";
+console.log(myVar);
