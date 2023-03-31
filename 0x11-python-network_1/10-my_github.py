7e762ae03d6007210159f94b26e22054f58e5c4a
simretandarge <andargesimret@gmail.com>
2023-03-31 14:27:12 -0700

    tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Python script that takes GitHub credentials (username and
-password) and uses the GitHub API to display the user id."""
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
-import requests
-from sys import argv
 
 if __name__ == "__main__":
-    auth = (argv[1], argv[2])
-    request = requests.get("https://api.github.com/user", auth=auth)
-    print(request.json().get("id"))
+    from requests import get
+    from sys import argv
+
+    r = get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status."""
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
-import requests
 
 if __name__ == "__main__":
-    content = requests.get("https://intranet.hbtn.io/status").text
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+    from sys import argv
+
+    try:
+        with urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as error:
+        print('Error code:', error.code)
